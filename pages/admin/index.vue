ba5c702c72f7bdfa3c7c181605abcbc3f53b925f
jackheuberger <7830838+jackheuberger@users.noreply.github.com>
2023-08-27 18:07:42 -0500

    create admin page (#1)

---
+++ pages/admin/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const supabase = useSupabaseClient()
+const user = useSupabaseUser()
+
+const role = ref('')
+
+let {data} = await supabase.from('profiles').select(`role`).eq('id', user.value.id).single()
+
+if (data) {
+    if (data.role !== 'admin') {
+        await navigateTo('/account')
+    }
+}
+
+</script>
+<template>
+<AdminComponent />
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+const user = useSupabaseUser()
+
+if (user.value) {
+    await navigateTo('/account')
+}
+
+</script>
 <template>
     <div class="prose prose-lg mt-10">
         <h1>Confirm your email</h1>
