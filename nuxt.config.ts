edfa6d874f721a41eabad67346efce2767543d15
jackheuberger <7830838+jackheuberger@users.noreply.github.com>
2023-08-27 18:16:36 -0500

    fix redirect (#2)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,13 @@
 export default defineNuxtConfig({
-  
-  modules: ['@nuxtjs/supabase', '@nuxtjs/tailwindcss'],
-  css: ['@/assets/main.css'],
-  supabase: {
-    redirectOptions: {
-      login: '/',
-      callback: '/confirm',
-      exclude: [],
-    }  
-  },
-  ssr: false
+
+    modules: ['@nuxtjs/supabase', '@nuxtjs/tailwindcss'],
+    css: ['@/assets/main.css'],
+    supabase: {
+        redirectOptions: {
+            login: '/',
+            callback: '/confirm',
+            exclude: ['/'],
+        }
+    },
+    ssr: false
 })
