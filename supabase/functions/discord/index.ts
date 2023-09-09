36ddbc4e253d06076466c4dbd25b4df0f166f26e
Jack Heuberger <jackheuberger@wustl.edu>
2023-09-08 23:26:50 -0500

    Finished discord verification bot implementation

--- supabase/functions/discord/index.ts
+++ supabase/functions/discord/index.ts
@@ -1,6 +1,3 @@
-// Sift is a small routing library that abstracts away details like starting a
-// listener on a port, and provides a simple function (serve) that has an API
-// to invoke a function for a specific path.
 import {
   json,
   serve,
@@ -9,6 +6,7 @@ import {
 // TweetNaCl is a cryptography library that we use to verify requests
 // from Discord.
 import nacl from "https://cdn.skypack.dev/tweetnacl@v1.0.3?dts";
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
 enum DiscordCommandType {
   Ping = 1,
@@ -46,10 +44,10 @@ async function home(request: Request) {
     )
   }
 
-  const { type = 0, data = { options: [] } } = JSON.parse(body)
+  const commandData = JSON.parse(body)
   // Discord performs Ping interactions to test our application.
   // Type 1 in a request implies a Ping interaction.
-  if (type === DiscordCommandType.Ping) {
+  if (commandData.type === DiscordCommandType.Ping) {
     return json({
       type: 1, // Type 1 in a response is a Pong interaction response type.
     })
@@ -57,16 +55,69 @@ async function home(request: Request) {
 
   // Type 2 in a request is an ApplicationCommand interaction.
   // It implies that a user has issued a command.
-  if (type === DiscordCommandType.ApplicationCommand) {
-    const { value } = data.options.find(
-      (option: { name: string; value: string }) => option.name === 'name'
-    )
+  if (commandData.type === DiscordCommandType.ApplicationCommand) {
+    try {
+      const supabase = createClient(
+        // Supabase API URL - env var exported by default.
+        Deno.env.get('SUPABASE_URL') ?? '',
+        // Supabase API ANON KEY - env var exported by default.
+        Deno.env.get('SUPABASE_ANON_KEY') ?? '',
+        // Create client with Auth context of the user that called the function.
+        // This way your row-level-security (RLS) policies are applied.
+        { global: { headers: { Authorization: request.headers.get('Authorization')! } } }
+      )
+
+      const user_email = commandData.data.options.find(
+        (option: { name: string; value: string }) => option.name === 'email'
+      ).value
+
+      const user_key = commandData.data.options.find(
+        (option: { name: string; value: string }) => option.name === 'secret_key'
+      ).value
+
+      let SBQuery = await supabase.from('profiles').select('email, secret_key').eq('email', user_email).single()
+
+      if (SBQuery.data.secret_key === user_key) {
+        // Make post request to update user role
+        
+        await DiscordRequest(`guilds/${commandData.guild_id}/members/${commandData.member.user.id}/roles/${Deno.env.get('ROLE_ID')}`, {method: "PUT"})
+
+        return json({
+          // Type 4 responds with the below message retaining the user's
+          // input at the top.
+          type: 4,
+          data: {
+            content: `Correct!!`,
+          },
+        })
+      } else {
+        return json({
+          // Type 4 responds with the below message retaining the user's
+          // input at the top.
+          type: 4,
+          data: {
+            content: `Incorrect!`,
+          },
+        })
+      }
+
+    } catch (error) {
+      return json({
+        // Type 4 responds with the below message retaining the user's
+        // input at the top.
+        type: 4,
+        data: {
+          content: `Error: ${error.message}!`,
+        },
+      })
+    }
+
     return json({
       // Type 4 responds with the below message retaining the user's
       // input at the top.
       type: 4,
       data: {
-        content: `Hello, ${JSON.stringify(data)}!`,
+        content: `Error!`,
       },
     })
   }
@@ -95,4 +146,28 @@ async function verifySignature(request: Request): Promise<{ valid: boolean; body
 /** Converts a hexadecimal string to Uint8Array. */
 function hexToUint8Array(hex: string) {
   return new Uint8Array(hex.match(/.{1,2}/g)!.map((val) => parseInt(val, 16)))
+}
+
+async function DiscordRequest(endpoint, options) {
+  // append endpoint to root API URL
+  const url = 'https://discord.com/api/v10/' + endpoint;
+  // Stringify payloads
+  if (options.body) options.body = JSON.stringify(options.body);
+  // Use node-fetch to make requests
+  const res = await fetch(url, {
+    headers: {
+      Authorization: `Bot ${Deno.env.get('DISCORD_BOT_TOKEN')}`,
+      'Content-Type': 'application/json; charset=UTF-8',
+      'User-Agent': 'Hack WashU Discord Bot',
+    },
+    ...options
+  });
+  // throw API errors
+  if (!res.ok) {
+    const data = await res.json();
+    console.log(res.status);
+    throw new Error(JSON.stringify(data));
+  }
+  // return original response
+  return res;
 }
