95106e676466b3c261d4d6e7f13fae3dc8cb9b4f
Vinay <v.vinay@wustl.edu>
2023-10-06 13:32:54 -0500

    Update

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,56 +1,82 @@
 <script setup lang="ts">
-const user = useSupabaseUser()
-const supabase = useSupabaseClient()
-const role = ref('')
+const user = useSupabaseUser();
+const supabase = useSupabaseClient();
+const role = ref("");
 
 onMounted(async () => {
-    if (user.value){
-        let {data} = await supabase.from('profiles').select(`role`).eq('id', user.value.id).single()
-        if (data)
-            role.value = data.role
-    }
-})
-
+  if (user.value) {
+    let { data } = await supabase
+      .from("profiles")
+      .select(`role`)
+      .eq("id", user.value.id)
+      .single();
+    if (data) role.value = data.role;
+  }
+});
 </script>
 <template>
-    <div class="navbar bg-base-300">
-        <div class="navbar-start">
-            <div class="dropdown">
-            <label tabindex="0" class="btn btn-ghost lg:hidden">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-            </label>
-            <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                <li><a href="/account">Account</a></li>
-                <li>
-                <a>Resources</a>
-                <ul class="p-2">
-                    <li><a href="/spaces">Space Information</a></li>
-                    <li><a>Submenu 2</a></li>
-                </ul>
-                </li>
-                <li><a href="/faq">FAQ</a></li>
-                <li v-if="role === 'admin'"> <a class="btn btn-primary" href="/admin">Admin Page</a> </li>
+  <div class="navbar bg-base-300">
+    <div class="navbar-start">
+      <div class="dropdown">
+        <label tabindex="0" class="btn btn-ghost lg:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h8m-8 6h16"
+            />
+          </svg>
+        </label>
+        <ul
+          tabindex="0"
+          class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          <li><a href="/account">Account</a></li>
+          <li>
+            <a>Resources</a>
+            <ul class="p-2">
+              <li><a href="/spaces">Space Information</a></li>
+              <li><a href="/schedule">Schedule</a></li>
+              <li><a href="/resources">Project Resources</a></li>
             </ul>
-            </div>
-            <a class="btn btn-ghost normal-case text-xl" href="/account">Hack WashU</a>
-        </div>
-        <div class="navbar-center hidden lg:flex">
-            <ul class="menu menu-horizontal px-1">
-            <li><a href="/account">Account</a></li>
-            <li tabindex="0">
-                <details>
-                <summary>Resources</summary>
-                <ul class="p-2">
-                    <li><a href="/spaces">Space Information</a></li>
-                    <li><a>Submenu 2</a></li>
-                </ul>
-                </details>
-            </li>
-            <li><a href="/faq">FAQ</a></li>
-            </ul>
-        </div>
-        <div class="navbar-end">
-            <a class="btn btn-primary" v-if="role === 'admin'" href="/admin">Admin Page</a>
-        </div>
+          </li>
+          <li><a href="/faq">FAQ</a></li>
+          <li v-if="role === 'admin'">
+            <a class="btn btn-primary" href="/admin">Admin Page</a>
+          </li>
+        </ul>
+      </div>
+      <a class="btn btn-ghost normal-case text-xl" href="/account"
+        >Hack WashU</a
+      >
     </div>
+    <div class="navbar-center hidden lg:flex">
+      <ul class="menu menu-horizontal px-1">
+        <li><a href="/account">Account</a></li>
+        <li tabindex="0">
+          <details>
+            <summary>Resources</summary>
+            <ul class="p-2">
+              <li><a href="/spaces">Space Information</a></li>
+              <li><a href="/schedule">Schedule</a></li>
+              <li><a href="/resources">Project Resources</a></li>
+            </ul>
+          </details>
+        </li>
+        <li><a href="/faq">FAQ</a></li>
+      </ul>
+    </div>
+    <div class="navbar-end">
+      <a class="btn btn-primary" v-if="role === 'admin'" href="/admin"
+        >Admin Page</a
+      >
+    </div>
+  </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,29 @@
 export default defineNuxtConfig({
-    runtimeConfig: {
-        public: {
-            siteUrl: process.env.NUXT_ENV_VERCEL_URL ? `https://app.hackwashu.com` : 'https://localhost:3000',
-        }
+  runtimeConfig: {
+    public: {
+      siteUrl: process.env.NUXT_ENV_VERCEL_URL
+        ? `https://app.hackwashu.com`
+        : "https://localhost:3000",
     },
-    modules: ['@nuxtjs/supabase', '@nuxt/content', '@vueuse/nuxt', '@nuxtjs/color-mode', '@nuxtjs/tailwindcss'],
-    css: ['@/assets/main.css'],
-    supabase: {
-        redirectOptions: {
-            login: '/',
-            callback: '/confirm',
-            exclude: ['/', '/faq', '/spaces'],
-        }
+  },
+  modules: [
+    "@nuxtjs/supabase",
+    "@nuxt/content",
+    "@vueuse/nuxt",
+    "@nuxtjs/color-mode",
+    "@nuxtjs/tailwindcss",
+  ],
+  css: ["@/assets/main.css"],
+  supabase: {
+    redirectOptions: {
+      login: "/",
+      callback: "/confirm",
+      exclude: ["/", "/faq", "/spaces", "/schedule", "/resources"],
     },
-    colorMode: {
-        preference: 'dracula',
-        dataValue: 'theme'
-    },
-    ssr: false
-})
+  },
+  colorMode: {
+    preference: "dracula",
+    dataValue: "theme",
+  },
+  ssr: false,
+});
