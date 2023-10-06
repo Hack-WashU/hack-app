44f2a2a011df1597dd076473f44ba0fdfa6a35a9
Jack Heuberger <jackheuberger@wustl.edu>
2023-10-06 15:02:32 -0500

    Add events, text callout

--- components/VerifyInfo.vue
+++ components/VerifyInfo.vue
@@ -10,4 +10,5 @@ import {PhDiscordLogo} from '@phosphor-icons/vue'
           <div class="text-xs">If you need help during the event, open a ticket in the #event-helpme channel</div>
         </div>
     <a class="btn btn-warning btn-sm" href="https://discord.gg/VZmT3Zzeb2">Discord</a>
-</div></template>
+  </div>
+</template>
--- pages/schedule.vue
+++ pages/schedule.vue
@@ -1,4 +1,6 @@
 <script setup>
+import {PhPhone} from '@phosphor-icons/vue'
+
 const { data } = await useAsyncData("schedule", () =>
   queryContent("/schedule").findOne()
 );
@@ -9,10 +11,14 @@ const { data } = await useAsyncData("schedule", () =>
     <div class="prose prose-lg mt-10 text-center">
       <h1 class="text-center">Event Schedule 📆</h1>
       <p class="text-center">
-        This schedule will be updated as the event progresses. Please keep an
-        eye out on discord and text "@HWU23" to 81010 for push notifications
-        whenever there is an update
+        This schedule will be updated as the event progresses. Keep an eye on Discord!
       </p>
+      <div class="alert shadow-lg w-100">
+        <ph-phone :size="40"/>
+            <div>
+              <div class="text-md">Want text updates? Text "@hwu23" (without the quotes) to 81010!</div>
+            </div>
+      </div>
       <ContentRenderer :value="data" />
     </div>
   </div>
