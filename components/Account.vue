3c31c9c48bfeb39e7b337f99a278ad59cf2c4dd3
Vinay <v.vinay@wustl.edu>
2023-10-11 11:17:46 -0500

    update shirt

--- components/Account.vue
+++ components/Account.vue
@@ -1,102 +1,126 @@
 <script setup lang="ts">
-const supabase = useSupabaseClient()
+const supabase = useSupabaseClient();
 
-const loading = ref(true)
-const email = ref('')
-const checked_in = ref(false)
-const shirt = ref('M')
+const loading = ref(true);
+const email = ref("");
+const checked_in = ref(false);
+const shirt = ref("M");
 
-loading.value = true
-const user = useSupabaseUser()
+loading.value = true;
+const user = useSupabaseUser();
 
 let { data } = await supabase
-  .from('profiles')
+  .from("profiles")
   .select(`email, checked_in`)
-  .eq('id', user.value.id)
-  .single()
+  .eq("id", user.value.id)
+  .single();
 
 if (data) {
-  email.value = data.email
-  checked_in.value = data.checked_in
+  email.value = data.email;
+  checked_in.value = data.checked_in;
 }
 
 let shirtdata = await supabase
-  .from('shirts')
+  .from("shirts")
   .select(`shirt`)
-  .eq('email', data!.email)
-  .single()
+  .eq("email", data!.email)
+  .single();
 
 if (shirtdata.data) {
-  if (shirtdata.data.shirt === 'XS' || shirtdata.data.shirt === 'S' || shirtdata.data.shirt === 'M' || shirtdata.data.shirt === 'L' || shirtdata.data.shirt === 'XL') {
-    shirt.value = shirtdata.data.shirt
+  if (
+    shirtdata.data.shirt === "XS" ||
+    shirtdata.data.shirt === "S" ||
+    shirtdata.data.shirt === "M" ||
+    shirtdata.data.shirt === "L" ||
+    shirtdata.data.shirt === "XL"
+  ) {
+    shirt.value = shirtdata.data.shirt;
   } else {
-    shirt.value = 'ERROR'
+    shirt.value = "ERROR";
   }
-} 
-else {
-    shirt.value = 'ERROR'
+} else {
+  shirt.value = "ERROR";
 }
 
-loading.value = false
+loading.value = false;
 
 async function updateProfile() {
   try {
-    loading.value = true
-    const user = useSupabaseUser()
+    loading.value = true;
+    const user = useSupabaseUser();
 
     const updates = {
       updated_at: new Date(),
-    }
+    };
 
-    let { error } = await supabase.from('profiles').update(updates).eq('id', user.value.id)
-    if (error) alert(error.message)
+    let { error } = await supabase
+      .from("profiles")
+      .update(updates)
+      .eq("id", user.value.id);
+    if (error) alert(error.message);
   } catch (error: any) {
-    alert(error.message)
+    alert(error.message);
   } finally {
-    loading.value = false
+    loading.value = false;
   }
 }
 
 async function signOut() {
-  loading.value = true
-  await supabase.auth.signOut()
-  user.value = null
-  loading.value = false
-  await navigateTo('/')
+  loading.value = true;
+  await supabase.auth.signOut();
+  user.value = null;
+  loading.value = false;
+  await navigateTo("/");
 }
 
 // Determine card's background color based on shirt size
 const determineBackgroundColor = () => {
-  if (shirt.value === 'XS') {
-    return 'btn-danger'
-  } else if (shirt.value === 'S') {
-    return 'btn-secondary'
-  } else if (shirt.value === 'M') {
-    return 'btn-info'
-  } else if (shirt.value === 'L') {
-    return 'btn-accent'
-  } else if (shirt.value === 'XL') {
-    return 'btn-warning'
+  if (shirt.value === "XS") {
+    return "btn-danger";
+  } else if (shirt.value === "S") {
+    return "btn-secondary";
+  } else if (shirt.value === "M") {
+    return "btn-info";
+  } else if (shirt.value === "L") {
+    return "btn-accent";
+  } else if (shirt.value === "XL") {
+    return "btn-warning";
   } else {
-    return 'bg-primary'
+    return "bg-primary";
   }
-}
-const staticFormClass = "bg-neutral card w-96 shadow-xl mx-auto text-center items-center "
+};
+const staticFormClass =
+  "bg-neutral card w-96 shadow-xl mx-auto text-center items-center ";
 </script>
 
 <template>
   <form :class="[staticFormClass]" @submit.prevent="updateProfile">
     <div class="form-control">
       <label for="email">Email</label>
-      <input id="email" type="text" :value="email" class="input input-bordered w-full max-w-xs" disabled />
+      <input
+        id="email"
+        type="text"
+        :value="email"
+        class="input input-bordered w-full max-w-xs"
+        disabled
+      />
     </div>
     <div class="form-control">
-      <label for="shirt">Shirt Size</label>
-      <input id="shirt" type="text" :value="shirt" class="input input-bordered w-full max-w-xs" disabled />
+      <h4>Shirt Size</h4>
+      <p class="text-center">
+        {{ shirt }}
+      </p>
     </div>
 
     <div class="m-5">
-      <button class="btn" :class="determineBackgroundColor()" @click="signOut" :disabled="loading">Sign Out</button>
+      <button
+        class="btn"
+        :class="determineBackgroundColor()"
+        @click="signOut"
+        :disabled="loading"
+      >
+        Sign Out
+      </button>
     </div>
   </form>
 </template>
