3308c99687608eaf1fa00e502579b6c1564e8867
Jack Heuberger <jackheuberger@wustl.edu>
2023-10-12 18:12:46 -0500

    fix

--- pages/confirm.vue
+++ pages/confirm.vue
@@ -4,10 +4,6 @@ const supabase = useSupabaseClient()
 
 watch(user, async () => {
   if (user.value) {
-    let {data} = await supabase.from('profiles').select(`checked_in`).eq('id', user.value.id).single()
-    if(data && !data.checked_in) {
-      return navigateTo('/checkin')
-    }
     return navigateTo('/account')
   }
 }, { immediate: true })
